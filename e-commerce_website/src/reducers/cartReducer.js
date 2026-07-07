b564aeb33e05773b0ea9228029d5eaf371862f24
yashsb-07 <yashsbansode07@gmail.com>
2026-07-07 18:10:39 +0530

    build cart reducer

---
+++ e-commerce_website/src/reducers/cartReducer.js
@@ -0,0 +1,56 @@
+export const CART_ACTIONS = {
+  ADD_TO_CART: "ADD_TO_CART",
+  REMOVE_FROM_CART: "REMOVE_FROM_CART",
+  INCREASE_QUANTITY: "INCREASE_QUANTITY",
+  DECREASE_QUANTITY: "DECREASE_QUANTITY",
+  CLEAR_CART: "CLEAR_CART",
+};
+
+export function cartReducer(state, action) {
+  switch (action.type) {
+    case CART_ACTIONS.ADD_TO_CART: {
+        const product = action.payload;
+
+        // Check if product already exists
+        const existingProduct = state.find(
+            (item) => item.id === product.id
+        );
+
+        // If product exists, increase quantity
+        if (existingProduct) {
+            return state.map((item) =>
+            item.id === product.id
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                }
+                : item
+            );
+        }
+
+        // Otherwise add new product
+        return [
+            ...state,
+            {
+            ...product,
+            quantity: 1,
+            },
+        ];
+        }
+
+    case CART_ACTIONS.REMOVE_FROM_CART:
+      return state;
+
+    case CART_ACTIONS.INCREASE_QUANTITY:
+      return state;
+
+    case CART_ACTIONS.DECREASE_QUANTITY:
+      return state;
+
+    case CART_ACTIONS.CLEAR_CART:
+      return [];
+
+    default:
+      return state;
+  }
+}
