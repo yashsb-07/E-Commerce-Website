b9d26b9b3533ede2bfc4c2dd23b3991f4c08fcc7
yashsb-07 <yashsbansode07@gmail.com>
2026-07-07 18:51:21 +0530

    Wrap the Application with CartProvider in main.jsx

---
+++ e-commerce_website/src/hooks/useCart.js
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { CartContext } from "../context";
+
+export function useCart() {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error(
+      "useCart must be used within a CartProvider"
+    );
+  }
+
+  return context;
+}
--- e-commerce_website/src/main.jsx
+++ e-commerce_website/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
