102568f463e0546e8d54e43466e993e77765cd5e
yashsb-07 <yashsbansode07@gmail.com>
2026-07-10 12:17:35 +0530

    feat(cart): create responsive cart page layout

--- e-commerce_website/src/pages/Cart.jsx
+++ e-commerce_website/src/pages/Cart.jsx
@@ -1,24 +1,20 @@
-import "./../components/cart/Cart.css";
+import "../components/cart/Cart.css";
 
 const Cart = () => {
     return (
         <section className="cart-page">
             <div className="container">
-
                 <h1>Shopping Cart</h1>
 
                 <div className="cart-container">
-
                     <div className="cart-items">
-
+                        {/* Cart items will be added in the next steps */}
                     </div>
 
                     <div className="cart-summary">
-
+                        {/* Order summary will be added later */}
                     </div>
-
                 </div>
-
             </div>
         </section>
     );
