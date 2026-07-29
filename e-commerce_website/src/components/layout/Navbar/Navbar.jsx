73c60540ffeea9083d04d8f412713b48d32e0baf
yashsb-07 <yashsbansode07@gmail.com>
2026-07-29 20:47:02 +0530

    feat(wishlist): integrate wishlist count into navbar

--- e-commerce_website/src/components/layout/Navbar/Navbar.jsx
+++ e-commerce_website/src/components/layout/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
 import "./Navbar.css";
+
 import { Link } from "react-router-dom";
-import { FaSearch, FaShoppingCart, FaUser, FaBars } from "react-icons/fa";
+import {
+  FaSearch,
+  FaHeart,
+  FaShoppingCart,
+  FaUser,
+  FaBars,
+} from "react-icons/fa";
+
 import { useCart } from "../../../hooks/useCart";
+import { useWishlist } from "../../../hooks/useWishlist";
 
 function Navbar() {
-
   const { totalItems } = useCart();
+  const { wishlistCount } = useWishlist();
 
   return (
     <nav className="navbar">
       <div className="container navbar-container">
+
         {/* Logo */}
         <div className="logo">
           <Link to="/">ShopEase</Link>
@@ -17,17 +27,53 @@ function Navbar() {
 
         {/* Nav Links */}
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="/wishlist">Wishlist</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+
+          <li>
+            <Link to="/wishlist">Wishlist</Link>
+          </li>
+
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
         </ul>
 
         {/* Icons */}
         <div className="nav-icons">
           <FaSearch />
+
+          {/* Wishlist */}
+          <div className="wishlist-icon-wrapper">
+            <Link
+              to="/wishlist"
+              aria-label={`Wishlist with ${wishlistCount} ${
+                wishlistCount === 1 ? "product" : "products"
+              }`}
+            >
+              <FaHeart />
+            </Link>
+
+            {wishlistCount > 0 && (
+              <span className="wishlist-badge">
+                {wishlistCount}
+              </span>
+            )}
+          </div>
+
+          {/* Cart */}
           <div className="cart-icon-wrapper">
-            <Link to="/cart">
+            <Link
+              to="/cart"
+              aria-label={`Cart with ${totalItems} ${
+                totalItems === 1 ? "item" : "items"
+              }`}
+            >
               <FaShoppingCart />
             </Link>
 
@@ -37,7 +83,14 @@ function Navbar() {
               </span>
             )}
           </div>
-          <Link to="/profile"><FaUser /></Link>
+
+          <Link
+            to="/profile"
+            aria-label="Profile"
+          >
+            <FaUser />
+          </Link>
+
           <FaBars className="menu-toggle" />
         </div>
       </div>
